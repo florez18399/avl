63a508a951e9f9a967924d02a88a27aeb987f739
Andres Florez <andres.florez03@uptc.edu.co>
2018-04-10 08:38:54 -0500

    Agregados los casos de balanceo LR y RL. Método de balanceo completado.

--- src/models/Tree.java
+++ src/models/Tree.java
@@ -31,54 +31,56 @@ public class Tree {
 		else
 			throw new NullPointerException("El nodo con la información especificada ya se encuentra");
 	}
-	
+
 	public void balanceTree() {
-		if(root.getBalanceFactor() < -1) {
+		if (root.getBalanceFactor() < -1) {
+			if (root.getRight().getLeft() != null)
+				balanceRR(root.getRight());
 			balanceLL(root);
-		}else if(root.getBalanceFactor() > 1) {
+		} else if (root.getBalanceFactor() > 1) {
+			if (root.getLeft().getRight() != null) 
+				balanceLL(root.getLeft());
 			balanceRR(root);
 		}
 	}
-	
+
 	public void balanceRR(Node nodeToBalance) {
 		changeRightRR(nodeToBalance);
 		changeLeftRR(nodeToBalance);
 	}
-	
+
 	private void changeLeftRR(Node nodeToBalance) {
-		while(nodeToBalance.getLeft().getLeft() != null) {
+		while (nodeToBalance.getLeft().getLeft() != null) {
 			nodeToBalance.setInfo(nodeToBalance.getLeft().getInfo());
 			nodeToBalance = nodeToBalance.getLeft();
 		}
 		nodeToBalance.setInfo(nodeToBalance.getLeft().getInfo());
 		nodeToBalance.setLeft(null);
 	}
-	
+
 	private void changeRightRR(Node nodeToBalance) {
 		int infoAux = nodeToBalance.getInfo();
-		while(nodeToBalance.getRight() != null) {
+		while (nodeToBalance.getRight() != null) {
 			infoAux = nodeToBalance.getRight().getInfo();
 			nodeToBalance.getRight().setInfo(nodeToBalance.getInfo());
-			if(nodeToBalance.getRight().getRight() != null)
+			if (nodeToBalance.getRight().getRight() != null)
 				nodeToBalance.getRight().getRight().setInfo(infoAux);
 			nodeToBalance = nodeToBalance.getRight();
 		}
 		nodeToBalance.setRight(new Node(infoAux));
 	}
-	
-	
 
 	public void balanceLL(Node nodeToBalance) {
 		changeLeftLL(nodeToBalance);
 		changeRightLL(nodeToBalance);
 	}
-	
+
 	private void changeLeftLL(Node nodeToBalance) {
 		int infoAux = nodeToBalance.getInfo();
-		while(nodeToBalance.getLeft() != null) {
+		while (nodeToBalance.getLeft() != null) {
 			infoAux = nodeToBalance.getLeft().getInfo();
 			nodeToBalance.getLeft().setInfo(nodeToBalance.getInfo());
-			if(nodeToBalance.getLeft().getLeft() != null)
+			if (nodeToBalance.getLeft().getLeft() != null)
 				nodeToBalance.getLeft().getLeft().setInfo(infoAux);
 			nodeToBalance = nodeToBalance.getLeft();
 		}
@@ -86,7 +88,7 @@ public class Tree {
 	}
 
 	private void changeRightLL(Node nodeToBalance) {
-		while(nodeToBalance.getRight().getRight() != null) {
+		while (nodeToBalance.getRight().getRight() != null) {
 			nodeToBalance.setInfo(nodeToBalance.getRight().getInfo());
 			nodeToBalance = nodeToBalance.getRight();
 		}
